0420f0d5db73f8de678489a6f124ce120fd1ee60
Mohammad Rouf <mrouf82@gmail.com>
2021-02-28 22:49:23 -0500

    added initial files

---
+++ index.js
@@ -0,0 +1,46 @@
+// TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+// TODO: Create an array of questions for user input
+const questions = () =>
+  inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is your name?",
+    },
+    {
+      type: "input",
+      name: "location",
+      message: "Where are you from?",
+    },
+    {
+      type: "input",
+      name: "hobby",
+      message: "What is your favorite hobby?",
+    },
+    {
+      type: "input",
+      name: "food",
+      message: "What is your favorite food?",
+    },
+    {
+      type: "input",
+      name: "github",
+      message: "Enter your GitHub Username",
+    },
+    {
+      type: "input",
+      name: "linkedin",
+      message: "Enter your LinkedIn URL.",
+    },
+  ]);
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
