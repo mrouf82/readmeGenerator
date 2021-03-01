330044902a169de8fd2537bbfc8df7705f64ed94
Mohammad Rouf <mrouf82@gmail.com>
2021-03-01 00:27:31 -0500

    fixed small bug

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-// TODO: Include packages needed for this application
+// Include packages needed for this application
 const inquirer = require("inquirer");
 const fs = require("fs");
-// TODO: Create an array of questions for user input
+//file that contains the markdown of the readMe user will create
+const createReadme = require("./utils/generateMarkdown");
+//Array of questions for user input
 const questions = () =>
   inquirer.prompt([
     {
@@ -36,21 +38,18 @@ const questions = () =>
     },
   ]);
 
-// TODO: Create a function to write README file
-function createReadme(fileName, data) {}
-
 // TODO: Create a function to initialize app
-function init() () => {
+function init() {
   questions().then((answers) => {
     try {
       const md = createReadme(answers);
-      fs.writeFileSync('./Example/README.md', md);
-      console.log('Successfully created README');
+      fs.writeFileSync("./Example/README.md", md);
+      console.log("Successfully created README");
     } catch (error) {
       console.log(error);
     }
   });
-};
+}
 
 // Function call to initialize app
 init();
